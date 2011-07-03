b7f52a53f565e35c3c3d1f3f3099c71e0f1d9eaf
Martin Liska <stubble@cgg.mff.cuni.cz>
2011-07-03 15:52:24 +0000

    Added the initial part of HairShape skeleton.

---
+++ Stubble/HairGuides.cpp
@@ -0,0 +1,9 @@
+#include "HairGuides.hpp"
+
+using namespace Stubble;
+using namespace HairShape;
+
+void HairGuides::generate(const MFnMesh &aMayaMesh, int aCount, bool aInterpolateFromPrevious)
+{
+
+}
