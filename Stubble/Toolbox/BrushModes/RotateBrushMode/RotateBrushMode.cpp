33654f553f71382dd98478d07cdfea74d5b777a2
Martin Kahoun <kahoun@cgg.mff.cuni.cz>
2011-11-19 17:35:20 +0000

    Fixed rotation and translation matrices creation code.

--- Stubble/Toolbox/BrushModes/RotateBrushMode/RotateBrushMode.cpp
+++ Stubble/Toolbox/BrushModes/RotateBrushMode/RotateBrushMode.cpp
@@ -48,7 +48,7 @@ void RotateBrushMode::doBrush ( HairTask *aTask )
 		if ( !mEnableFalloff )
 		{
 			R = getRotationMatrix( aTask->mDx.x, axisLocal );
-			A = T * R * Tinv; // Note that * is left associative
+			A = Tinv * (R * T); // Note that * is left associative
 		}
 
 		// Loop through all guide segments except the first one (that's a follicle and should not move)
@@ -61,7 +61,7 @@ void RotateBrushMode::doBrush ( HairTask *aTask )
 			if ( mEnableFalloff )
 			{
 				R = getRotationMatrix( aTask->mDx.x * guide->mSegmentsAdditionalInfo[ i ].mFallOff, axisLocal );
-				A = T * R * Tinv; // Note that * is left associative
+				A = Tinv * (R * T); // Note that * is left associative
 			}
 			
 			hairVertices[ i ] = Vector3D< Real >::transformPoint(hairVertices[ i ], A);
@@ -74,48 +74,54 @@ void RotateBrushMode::doBrush ( HairTask *aTask )
 
 Matrix< Real > RotateBrushMode::getRotationMatrix ( Real aMeasure, const Vector3D< Real > &aAxis )
 {
-	Real theta = 6.28 * aMeasure;
-	// R = cos a * I + (1 - cos a) * A1 + sin a * A2
-	const Real cosA = cos(theta); // Cosine alpha
-	const Real cCosA = 1.0 - cosA; // Cosine alpha complement
-	const Real sinA = sin(theta); // Sine alpha
-	Matrix< Real > IcosA, A1, A2; // Unit matrix multiplied by cos a and two matrices containing axis of rotation
+	// R = uu^T + (cos theta)(I - uu^T) + (sin theta)S, see OpenGL Red Book 2.0, appendix F
+	// u is unit vector of the axis, S is 3x3 matrix as defined bellow
+	Real theta = 3.14 * aMeasure;
+	const Real cosT = cos(theta); // Cosine theta
+	const Real sinT = sin(theta); // Sine theta
+	Matrix< Real > M1, M2, S;
 
-	// Unit matrix multiplied by cos alpha
-	IcosA[ 0 ] = cosA;
-	IcosA[ 5 ] = cosA;
-	IcosA[ 10 ] = cosA;
-	IcosA[ 15 ] = 1.0;
-
+	// M1 = uu^T
 	// First column
-	A1[ 0 ] = aAxis.x * aAxis.x * cCosA;
-	A1[ 1 ] = aAxis.x * aAxis.y * cCosA;
-	A1[ 2 ] = aAxis.x * aAxis.z * cCosA;
+	M1[ 0 ] = aAxis.x * aAxis.x;
+	M1[ 1 ] = aAxis.x * aAxis.y;
+	M1[ 2 ] = aAxis.x * aAxis.z;
 	// Second column
-	A1[ 4 ] = aAxis.y * aAxis.x * cCosA;
-	A1[ 5 ] = aAxis.y * aAxis.y * cCosA;
-	A1[ 6 ] = aAxis.y * aAxis.z * cCosA;
+	M1[ 4 ] = aAxis.y * aAxis.x;
+	M1[ 5 ] = aAxis.y * aAxis.y;
+	M1[ 6 ] = aAxis.y * aAxis.z;
 	// Third column
-	A1[ 8 ] = aAxis.z * aAxis.x * cCosA;
-	A1[ 9 ] = aAxis.z * aAxis.y * cCosA;
-	A1[ 10 ] = aAxis.z * aAxis.z * cCosA;
-	
-	// First column
-	A2[ 1 ] = aAxis.z * sinA;
-	A2[ 2 ] = -aAxis.y * sinA;
-	// Second column
-	A2[ 4 ] = -aAxis.z * sinA;
-	A2[ 6 ] = aAxis.x * sinA;
-	// Third column
-	A2[ 8 ] = aAxis.y * sinA;
-	A2[ 9 ] = -aAxis.x * sinA;
+	M1[ 8 ] = aAxis.z * aAxis.x;
+	M1[ 9 ] = aAxis.z * aAxis.y;
+	M1[ 10 ] = aAxis.z * aAxis.z;
 
-	//dumpToFile(IcosA);
-	//dumpToFile(A1);
-	//dumpToFile(A2);
+	// M2 = (cos theta)(I - M1)
+	// First column
+	M2[ 0 ] = cosT * (1.0 - M1[ 0 ]);
+	M2[ 1 ] = -cosT * M1[ 1 ];
+	M2[ 2 ] = -cosT * M1[ 2 ];
+	// Second column
+	M2[ 4 ] = -cosT * M1[ 4 ];
+	M2[ 5 ] = cosT * (1.0 - M1[ 5 ]);
+	M2[ 6 ] = -cosT * M1[ 6 ];
+	// Third column
+	M2[ 8 ] = -cosT * M1[ 8 ];
+	M2[ 9 ] = -cosT * M1[ 9 ];
+	M2[ 10 ] = cosT * (1.0 - M1[ 10 ]);
+
+	// S matrix
+	// First column
+	S[ 1 ] = sinT * aAxis.z;
+	S[ 2 ] = -sinT * aAxis.y;
+	// Second column
+	S[ 4 ] = -sinT * aAxis.z;
+	S[ 6 ] = sinT * aAxis.x;
+	// Third column
+	S[ 8 ] = sinT * aAxis.y;
+	S[ 9 ] = -sinT * aAxis.x;
 	
-	Matrix<Real> R = IcosA + A1 + A2;
-	//dumpToFile(R);
+	Matrix<Real> R = M1 + M2 + S;
+	R[ 15 ] = 1.0; // Make it proper homogenic 4x4 matrix
 	return R;
 }
 
@@ -144,19 +150,20 @@ void RotateBrushMode::getRotationAxis ( short aX, short aY, M3dView &aView, Vect
 
 void RotateBrushMode::getTranslationMatrices ( const Vector3D< Real > &aPosition, Matrix< Real > &aToOrigin, Matrix< Real > &aFromOrigin)
 {
+	// Transposed translate matrices because of the left-side multiplication
 	Matrix< Real > T;
 	T[ 0 ] = 1.0;
 	T[ 5 ] = 1.0;
 	T[ 10 ] = 1.0;
-	T[ 12 ] = -aPosition.x;
-	T[ 13 ] = -aPosition.y;
-	T[ 14 ] = -aPosition.z;
+	T[ 3 ] = aPosition.x;
+	T[ 7 ] = aPosition.y;
+	T[ 11 ] = aPosition.z;
 	T[ 15 ] = 1.0;
 
 	Matrix< Real > Tinv(T);
-	Tinv[ 12 ] = -Tinv[ 12 ];
-	Tinv[ 13 ] = -Tinv[ 13 ];
-	Tinv[ 14 ] = -Tinv[ 14 ];
+	Tinv[ 3 ] = -Tinv[ 3 ];
+	Tinv[ 7 ] = -Tinv[ 7 ];
+	Tinv[ 11 ] = -Tinv[ 11 ];
 
 	aToOrigin = T;
 	aFromOrigin = Tinv;
